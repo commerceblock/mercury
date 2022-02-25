65aea0e851331d98103756817dfc4039d9b53c0f
Tom Trevethan <tptrevethan@googlemail.com>
2022-02-25 12:57:58 +0000

    set initial swap groups to permitted

--- server/src/config.rs
+++ server/src/config.rs
@@ -44,9 +44,9 @@ pub struct ConductorConfig {
 impl Default for ConductorConfig {
     fn default() -> Self {
         Self {
-            group_timeout: 60,
+            group_timeout: 180,
             utxo_timeout: 10,
-            punishment_duration: 60,
+            punishment_duration: 180,
             daily_epochs: 240,
             max_swap_size: 5,
             swap_wallet_version: "0.6.0".to_string(),
